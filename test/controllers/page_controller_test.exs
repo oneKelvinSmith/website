f734bf1e10ad9e338a351b874ceda0cafee1ed41
Kelvin Smith <oneKelvinSmith@gmail.com>
2016-06-20 21:18:57 +0100

    Remove linkedin and add reasonable tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,8 +1,23 @@
 defmodule Me.PageControllerTest do
   use Me.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "GET / has a welcome message", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Welcome to my website"
+  end
+
+  test "GET / links to github", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "https://github.com/oneKelvinSmith"
+  end
+
+  test "GET / links to twitter", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "https://twitter.com/oneKelvinSmith"
+  end
+
+  test "GET / links to cv", %{conn: conn} do
+    conn = get conn, "/"
+    assert html_response(conn, 200) =~ "https://stackoverflow.com/cv/oneKelvinSmith"
   end
 end
